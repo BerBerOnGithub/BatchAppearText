2592d87caca17050cb5e15da31860b237cb12571
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-11 13:15:59 +0200

    Add files via upload

---
+++ BatchAppearText.bat
@@ -0,0 +1,72 @@
+:sus
+@echo off
+@echo T
+timeout /t 1 /nobreak > nul
+cls
+@echo Th
+timeout /t 1 /nobreak > nul
+cls
+@echo Thi
+timeout /t 1 /nobreak > nul
+cls
+@echo This
+timeout /t 1 /nobreak > nul
+cls
+@echo This 
+timeout /t 1 /nobreak > nul
+cls
+@echo This i
+timeout /t 1 /nobreak > nul
+cls
+@echo This is
+timeout /t 1 /nobreak > nul
+cls
+@echo This is 
+timeout /t 1 /nobreak > nul
+cls
+@echo This is a
+timeout /t 1 /nobreak > nul
+cls
+@echo This is ap
+timeout /t 1 /nobreak > nul
+cls
+@echo This is app
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appe
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appea
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appear
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appeari
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearin
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing 
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing t
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing te
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing tex
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing text
+timeout /t 1 /nobreak > nul
+cls
+@echo This is appearing text.
+timeout /t 1 /nobreak > nul
+cls
+goto sus
